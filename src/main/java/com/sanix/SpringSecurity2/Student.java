b7d92061cc8abdc55a08cde3622e80250cb44de8
sanix <sanicksikani@gmail.com>
2020-09-19 22:12:35 +0300

    Build API

---
+++ src/main/java/com/sanix/SpringSecurity2/Student.java
@@ -0,0 +1,20 @@
+package com.sanix.SpringSecurity2;
+
+public class Student {
+
+    private final Integer studentId;
+    private final String lastName;
+
+    public Student(Integer studentId, String lastName) {
+        this.studentId = studentId;
+        this.lastName = lastName;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
