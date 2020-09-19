e8f1c72a52f91c2750beb1ad2db6651ca81b9589
sanix <sanicksikani@gmail.com>
2020-09-19 22:53:30 +0300

    In memory user details manager

--- src/main/java/com/sanix/SpringSecurity2/security/ApplicationSecurityConfig.java
+++ src/main/java/com/sanix/SpringSecurity2/security/ApplicationSecurityConfig.java
@@ -1,7 +1,12 @@
 package com.sanix.SpringSecurity2.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -15,5 +20,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()//must be authenticated
                 .and()
                 .httpBasic();//using basic authentication
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService(){
+        UserDetails annaSmithUser= User.builder()
+                    .username("annasmith")
+                    .password("password")
+                    .roles("STUDENT") //ROLE_STUDENT
+                    .build();
+
+        return new InMemoryUserDetailsManager(
+                annaSmithUser
+        );
     }
 }
