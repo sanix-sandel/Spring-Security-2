f2178cb44a733c7dd9075418c375f6685ece7b39
sanix <sanicksikani@gmail.com>
2020-09-19 23:12:00 +0300

    Password encoding with Bcrypt

--- src/main/java/com/sanix/SpringSecurity2/security/ApplicationSecurityConfig.java
+++ src/main/java/com/sanix/SpringSecurity2/security/ApplicationSecurityConfig.java
@@ -2,13 +2,22 @@ package com.sanix.SpringSecurity2.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
@@ -27,7 +36,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected UserDetailsService userDetailsService(){
         UserDetails annaSmithUser= User.builder()
                     .username("annasmith")
-                    .password("password")
+                    .password(passwordEncoder.encode("password"))
                     .roles("STUDENT") //ROLE_STUDENT
                     .build();
 
