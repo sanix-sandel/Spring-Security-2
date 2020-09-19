c9aa0a37f6f625745c7e79144db588c8f6b497f8
sanix <sanicksikani@gmail.com>
2020-09-19 22:32:24 +0300

    Basic Auth

---
+++ src/main/java/com/sanix/SpringSecurity2/security/ApplicationSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.sanix.SpringSecurity2.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()//Authorize request
+                .anyRequest()//Any request
+                .authenticated()//must be authenticated
+                .and()
+                .httpBasic();//using basic authentication
+    }
+}
